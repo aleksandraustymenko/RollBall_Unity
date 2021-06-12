13dd0f3a9f0529063b0c9e79913a5e3f48f08c75
Oleksandra Ustymenko <oleksandra.ustymenko@student.uwm.edu.pl>
2021-06-12 19:12:12 +0200

    add game roll a ball

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,37 @@
+﻿using Unity.Mathematics;
+using Unity.Transforms;
+using Unity.Entities;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public static CameraFollow instance;
+
+    public Entity ballEntity;
+    public float3 offset;
+
+    private EntityManager manager;
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        manager = World.DefaultGameObjectInjectionWorld.EntityManager;
+    }
+
+    private void LateUpdate()
+    {
+        if (ballEntity == null) { return; }
+        Translation ballPos = manager.GetComponentData<Translation>(ballEntity);
+        transform.position = ballPos.Value + offset;
+    }
+
+
+
+}
+
---
+++ Assets/Scripts/Systems/MovementSystem.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using Unity.Entities;
+using Unity.Physics;
+using Unity.Mathematics;
+using Unity.Jobs;
+
+
+[AlwaysSynchronizeSystem]
+public class MovementSystem : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        float deltaTime = Time.DeltaTime;
+
+        float2 cutInput = new float2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        Entities.ForEach((ref PhysicsVelocity vel, in SpeedData speedData) =>
+        {
+            float2 newvel = vel.Linear.xz;
+
+            newvel += cutInput * speedData.speed * deltaTime;
+
+            vel.Linear.xz = newvel;
+        }).Run();
+
+        return default;
+    }
+}
